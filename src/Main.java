9722e0118e40d1e2300474a292851e2b706fd5a5
Хакер <ololollol@frkgkd.com>
2022-08-31 13:15:26 +0300

    дз переменные

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var box = 5;
+        System.out.println(box);
+        box = box + 2;
+        System.out.println(box);
+        box = box - 3;
+        System.out.println(box);
+        box = box * 3;
+        System.out.println(box);
+        box = box / 4;
+        System.out.println(box);
+
+        var liftingCapasity = 50;
+        var stuffWeight = 20;
+        var capacityLeft = liftingCapasity - stuffWeight;
+        System.out.println("Ещё можно положить " + capacityLeft + " кг вещей");
+
+        var appleWeight = 2;
+        var orangesWeight = 3;
+        var fruitWeight = appleWeight + orangesWeight;
+        System.out.println("общий вес фруктов " + fruitWeight);
+
+        var meatWeight = 4;
+        var waterWeight = 5;
+        var tomatoesWeight = 2;
+        var cucumbersWeight = 2;
+        var peppersWeight = 2;
+        var zucchinWeight = 2;
+        var vegetablesWeight = tomatoesWeight + cucumbersWeight + peppersWeight + zucchinWeight;
+        var productWeight = vegetablesWeight + meatWeight + waterWeight + fruitWeight;
+        System.out.println("Общий вес продуктов " + productWeight + " кг!");
+
+
     }
 }
